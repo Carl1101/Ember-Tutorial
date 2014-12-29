b2ba86aaf23fed03ea04fdeea65f9fc8c5d2f711
Carl1101 <carlosvazquez014@gmail.com>
2014-12-29 00:18:39 -0400

    Added Fixture Data for Testing

--- js/application.js
+++ js/application.js
@@ -1,4 +1,6 @@
-var Todos = Ember.Application.create();
+window.Todos = Ember.Application.create();
+
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' });
