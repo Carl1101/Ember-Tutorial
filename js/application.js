a8ca5f7bfb359fb86c5e32baff38bd422c609c13
Carl1101 <carlosvazquez014@gmail.com>
2014-12-29 00:31:03 -0400

    Now displays the data within the todo class dynamically

--- js/application.js
+++ js/application.js
@@ -5,3 +5,9 @@ Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' });
 });
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('todo');
+  }
+});
